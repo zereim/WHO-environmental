0c0821d60a75d60f748139f907bdcf6997b3eb9b
zereim <wtruskolaski@gmail.com>
2026-06-04 23:06:10 +0200

    creating a description

--- main.R
+++ main.R
@@ -1,5 +1,10 @@
 library(skimr)
 library(tidyverse)
+
+# the goal of this project will be to explore the relationships between deaths
+# attributable to the env and iso enviornmental certifications
+
+
 df_WHO <- read_csv("deaths_attributable_to_environment_WHO.csv")
 skim(df_WHO)
 df_WHO <- df_WHO[, colSums(!is.na(df_WHO)) > 0]
