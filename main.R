4d27d4f0ea52c4ff4ff0342a9b9079439162f84e
zereim <wtruskolaski@gmail.com>
2026-06-04 22:53:12 +0200

    changing a few things

--- main.R
+++ main.R
@@ -1,5 +1,9 @@
 library(skimr)
 library(tidyverse)
-df <- read_csv("deaths_attributable_to_environment_WHO.csv")
-skim(df)
-df <- df[, colSums(!is.na(df)) > 0]
+df_WHO <- read_csv("deaths_attributable_to_environment_WHO.csv")
+skim(df_WHO)
+df_WHO <- df_WHO[, colSums(!is.na(df_WHO)) > 0]
+
+df_iso <- read_csv("iso14000_country_per_industry_certifications.csv")
+skim(df_WHO)
+
